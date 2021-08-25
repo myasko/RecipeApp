c7752d497f3b5b01b2a982e4ffd553876c37b1fc
George <81197824+myasko@users.noreply.github.com>
2021-08-25 22:35:30 +0300

    Initial Commit

---
+++ RecipeApp/RecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppApp.swift
+//  RecipeApp
+//
+//  Created by Георгий Бутров on 25.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
